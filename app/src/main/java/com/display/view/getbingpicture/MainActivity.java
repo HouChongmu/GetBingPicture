6b9acd3a8994067227584501d2a87e0cd03e0baf
firstdream10 <firstdream10@hotmail.com>
2017-09-28 08:40:21 +0800

    初始化

---
+++ app/src/main/java/com/display/view/getbingpicture/MainActivity.java
@@ -0,0 +1,91 @@
+package com.display.view.getbingpicture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private static String bingUrl = "http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        Button getPicBtn = findViewById(R.id.get_picture);
+        getPicBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.get_picture:
+                getWithHttpUrlConn();
+                break;
+            case R.id.get_picture_okhttp:
+                getWithOkHttp();
+                break;
+        }
+
+    }
+
+    public void getWithHttpUrlConn() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL(bingUrl);
+                    connection = (HttpURLConnection)
+                            url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream inputStream = connection.getInputStream();
+                    reader = new BufferedReader(
+                            new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void getWithOkHttp() {
+
+    }
+}
